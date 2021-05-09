ce15a0720ea1b53834ee9bea5205b8a0c83715f7
Etienne Dldc <e.deladonchamps@gmail.com>
2021-05-09 14:02:14 +0200

    Upgrade

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
-const { app, BrowserWindow } = require('electron');
-const { parse: parseSql } = require('pg-query-parser');
-const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
-const { ipcMain: ipc } = require('electron');
+const { app, BrowserWindow } = require("electron");
+const { parse: parseSql } = require("pg-query-parser");
+const {
+  default: installExtension,
+  REACT_DEVELOPER_TOOLS,
+} = require("electron-devtools-installer");
+const { ipcMain: ipc } = require("electron");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -16,17 +19,18 @@ function createWindow() {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false,
     },
   });
 
   // and load the index.html of the app.
-  win.loadURL('http://localhost:1234');
+  win.loadURL("http://localhost:3000");
 
   // Open the DevTools.
   win.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  win.on("closed", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -37,18 +41,18 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
@@ -59,7 +63,7 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
-ipc.on('parse', async (event, query) => {
+ipc.on("parse", async (event, query) => {
   const res = parseSql(query);
-  event.sender.send('parse-response', res);
+  event.sender.send("parse-response", res);
 });
